56026b044c0ae7bf996fa276aa2b1dbc21f549dc
kai_Ivring <3011973335@qq.com>
2024-03-14 21:37:13 +0800

    24_3_14

---
+++ day01/learn.java
@@ -0,0 +1,34 @@
+package day01;//什么东东这玩意
+
+public class learn {
+    public static void main(String[] args) {
+      /*int age = 10;
+      String name = "mike";
+      System.out.println(age);
+      System.out.println(name); */
+
+      /* System.out.println(100 + 100);
+      System.out.println("haha" + "wow");
+      System.out.println(100 + 200 + "dunk"); */
+
+      /* long a = 10;
+      System.out.println(a); */
+
+      /* double a = 5.12e2;
+      double b = 5.12e-2;
+      System.out.println(a);
+      System.out.println(b); */
+
+      /* double a = 2.7;
+      double b = 8.1 / 3;
+      System.out.println(a);
+      System.out.println(b); 
+      if(Math.abs(a-b)<0.001)
+      {
+        System.out.println("a equals b\n");
+      } */
+
+      /* char a = '\t';
+      System.out.println(a); */
+    }
+}
