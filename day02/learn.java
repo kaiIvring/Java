259c36f977d66684805a70353afe7d3543f9cf5f
kai_Ivring <3011973335@qq.com>
2024-03-18 22:40:15 +0800

    24_3_18

---
+++ day02/learn.java
@@ -0,0 +1,10 @@
+package day02;
+public class learn{
+    public static void main(String args[]){
+        char a = 21556;
+        char b ='吴';
+        int c = b;
+        System.out.println(a);
+        System.out.println(c);
+    }
+}
