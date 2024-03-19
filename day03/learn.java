4eaf041035d93da6944c1413c43aca1683816116
kai_Ivring <3011973335@qq.com>
2024-03-19 21:40:20 +0800

    24_3_19

---
+++ day03/learn.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class learn{
+    public static void main(String args[]){
+        boolean a = true;
+        if(a == true)
+        {
+            System.out.println("true!");
+        }
+        else
+        {
+            System.out.println("False!");
+        }
+    }
+}
