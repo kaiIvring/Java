3e0714f27d449956248e3a0a7d0e8130ddbb767a
kai_Ivring <3011973335@qq.com>
2024-03-20 22:27:10 +0800

    24_3_20

---
+++ day04/learn.java
@@ -0,0 +1,30 @@
+package day04;
+public class learn{
+    public static void main(String[] args){
+/*         String s = "123";
+        int num = Integer.parseInt(s);
+        System.out.println(num); */
+
+/*         String sex = "male";
+        String name = "jack";
+        String hobby = "play";
+        System.out.println("name\tsex\thobby\n" + name + "\t" + sex + "\t" + hobby); */
+
+       /*  int num1 = 10 % 3; //1
+        int num2 = 10 % -3; //1
+        int num3 = -10 % 3; //-1
+        int num4 = -10 % -3; //-1
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4); */
+
+        /* int i = 1;
+        i = i++;
+        System.out.println(i); //1
+        int j = 1;
+        j = ++j;
+        System.out.println(j); */ //2
+
+    }
+}
